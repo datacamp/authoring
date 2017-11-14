2f2889a7dec8ca62d130e736ba3c6ec6a279243d
Goran <goran@datacamp.com>
2017-11-14 09:42:54 +0100

    Add _book to gitignore change the process to commit _book

--- jenkins/git-commit.sh
+++ jenkins/git-commit.sh
@@ -1,2 +1,9 @@
-git add -A
-git commit -m "Compiled"
+# A way to commit _book contents
+# Copy the .gitignore and remove the last line from it being _book
+# So we can add and commit
+cp .gitignore .gitignore.tmp
+sed '$ d' .gitignore.tmp > .gitignore
+rm -f .gitignore.tmp
+
+git add _book
+git commit -m "book compiled"
